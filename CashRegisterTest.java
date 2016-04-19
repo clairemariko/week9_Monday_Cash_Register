f47dec1a1309fada2efb918403b7a2a884348854
claire <claire.mariko.smith@gmail.com>
2016-04-19 08:29:59 +0100

    cash register

---
+++ CashRegisterTest.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;in
+
+
+
+/**
+   A class to test the CashRegister class.
+*/
+public class CashRegisterTest
+{
+   public static void main(String[] args)
+   {
+      CashRegister register = new CashRegister();
+      //register.recordPurchase(13.50);
+      Scanner in = new Scanner(System.in);
+      System.out.println("Price for first items: ");
+      double price1 = in.nextDouble();
+
+      System.out.println("Price for second items: ");
+      double price2 = in.nextDouble();
+
+      System.out.println("Payment amount: ");
+      double amount = in.nextDouble();
+
+      register.recordPurchase(price1);
+      register.recordPurchase(price2);
+      register.enterPayment(amount);
+      //register.recordPurchase(2.25);
+      //register.enterPayment(20);
+      double change = register.giveChange();
+      System.out.println(change);      
+   }
+}
